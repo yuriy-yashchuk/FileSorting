aa3b14e6de2ce7f7026ad26ed20963bfdfa5cfb8
Yuriy Yashchuk <yuriy.yashchuk@inoxoft.com>
2021-10-16 00:49:19 +0300

    Add project files.

---
+++ FileSorting/Program.cs
@@ -0,0 +1,155 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileSorting
+{
+    class Program
+    {
+        const string _folderPath = @"c:\Temp";
+        const string _inputFileName = "input.txt";
+        const string _outputFileName = "output.txt";
+        const int _chunkSizeLimit = 1000;//1000000000; //to keep chuck size around 1Gb
+
+        static async Task Main(string[] args)
+        {
+            var inputFilePath = $"{_folderPath}\\{_inputFileName}";
+            var outputFilePath = $"{_folderPath}\\{_outputFileName}";
+
+            Generate(inputFilePath);
+
+            var chunkFiles = await SplitFileAsync(inputFilePath);
+
+            MergeFilesAsync(chunkFiles, outputFilePath);
+
+            chunkFiles.ForEach(p => File.Delete(p));
+        }
+
+        private static void MergeFilesAsync(List<string> filePaths, string resultFilePath)
+        {
+            var readers = new List<StreamReader>();
+
+            foreach(var filepath in filePaths)
+            {
+                readers.Add(new StreamReader(filepath));
+            }
+
+            var writer = new StreamWriter(resultFilePath);
+            var all = readers.Select((reader, index) => (reader, reader.ReadLine())).ToList();
+
+            while (all.Count > 0)
+            {
+                all.Sort(CustomComparison);
+
+                var el = all.First();
+
+                writer.WriteLine(el.Item2);
+                
+                if (el.reader.EndOfStream)
+                {
+                    all.Remove(el);
+                }
+                else
+                {
+                    all[0] = (el.reader, el.reader.ReadLine());
+                }
+            }
+
+            readers.ForEach(r => r.Close());
+            writer.Close();
+        }
+
+        private static async Task<List<string>> SplitFileAsync(string filePath)
+        {
+            var resultFilePaths = new List<string>();
+            var reader = new StreamReader(filePath);
+
+            int chunkIndex = 0;
+
+            while (!reader.EndOfStream)
+            {
+                var chunkList = ReadChunk(reader);
+
+                chunkList.Sort(CustomComparison);
+
+                var resFilePath = $"{_folderPath}\\chunk{chunkIndex}.txt";
+                await File.WriteAllLinesAsync(resFilePath, chunkList);
+
+                chunkIndex++;
+                resultFilePaths.Add(resFilePath);
+            }
+
+            reader.Close();
+
+            return resultFilePaths;
+        }
+
+        private static List<string> ReadChunk(StreamReader reader)
+        {
+            var chunkList = new List<string>();
+            int chunkSize = 0;
+
+            while (!reader.EndOfStream && chunkSize < _chunkSizeLimit)
+            {
+                var line = reader.ReadLine();
+                chunkSize += line.Length + 2;//add 2 for \r\n
+                chunkList.Add(line);
+            }
+
+            return chunkList;
+        }
+
+        private static int CustomComparison(string a, string b)
+        {
+            (var aN, var aS) = ParseNumberAndString(a);
+            (var bN, var bS) = ParseNumberAndString(b);
+
+            var res = string.Compare(aS, bS);
+
+            if (res != 0)
+            {
+                return res;
+            }
+            else
+            {
+                if (aN == bN) return 0;
+                if (aN > bN) return 1;
+                return -1;
+            }
+        }
+
+        private static int CustomComparison<T>((T, string) a, (T, string) b)
+        {
+            return CustomComparison(a.Item2, b.Item2);
+        }
+
+        private static (int, string) ParseNumberAndString(string s)
+        {
+            var dotIndex = s.IndexOf('.');
+            var numberString = s.Substring(0, dotIndex);
+            if (!int.TryParse(numberString, out var number))
+            {
+                Console.WriteLine($"Error with {s}");
+            }
+
+            return (number, s[(dotIndex + 1)..]);
+        }
+
+        private static void Generate(string filePath)
+        {
+            var writer = new StreamWriter(filePath);
+            Random rnd = new Random();
+
+            for (int i = 0; i < 1000; i++)//100000000 for 1 Gb
+            {
+                var n = rnd.Next(0, 1000);
+                var str = $"{(char)rnd.Next(97, 122)}{(char)rnd.Next(97, 122)}{(char)rnd.Next(97, 122)}";
+                writer.WriteLine($"{n}. {str}");
+            }
+
+            writer.Close();
+        }
+    }
+}
